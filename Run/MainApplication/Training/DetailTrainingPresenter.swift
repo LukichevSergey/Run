5cd98f1601c32f8581e61a67744bf1d368a69ac5
EvgenLK <120525967+EvgenLK@users.noreply.github.com>
2023-10-30 22:18:25 +1000

    add

---
+++ Run/MainApplication/Training/DetailTrainingPresenter.swift
@@ -0,0 +1,48 @@
+//
+//  DetailTrainingPresenter.swift
+//  Run
+//
+//  Created by Evgenii Kutasov on 30.10.2023.
+//
+
+import Foundation
+import UIKit
+
+// MARK: Protocol - DetailTrainingViewToPresenterProtocol (View -> Presenter)
+protocol DetailTrainingViewToPresenterProtocol: AnyObject {
+    func viewDidLoad()
+}
+
+// MARK: Protocol - DetailTrainingInteractorToPresenterProtocol (Interactor -> Presenter)
+protocol DetailTrainingInteractorToPresenterProtocol: AnyObject {
+
+    func trainingIsFetchedWithError(error: Error)
+}
+
+class DetailTrainingPresenter {
+
+    // MARK: Properties
+    var router: DetailTrainingPresenterToRouterProtocol!
+    var interactor: DetailTrainingPresenterToInteractorProtocol!
+    weak var view: DetailTrainingPresenterToViewProtocol!
+    
+    weak var progressBar: ProgressBarViewProtocol?
+}
+
+// MARK: Extension - DetailTrainingViewToPresenterProtocol
+extension DetailTrainingPresenter: DetailTrainingViewToPresenterProtocol {
+    func viewDidLoad() {
+        logger.log("\(#fileID) -> \(#function)")
+        view.showActivityIndicator()
+    }
+}
+
+// MARK: Extension - DetailTrainingInteractorToPresenterProtocol
+extension DetailTrainingPresenter: DetailTrainingInteractorToPresenterProtocol {
+    
+    
+    func trainingIsFetchedWithError(error: Error) {
+        logger.log("\(#fileID) -> \(#function)")
+        view.removeActivityIndicator()
+    }
+}
---
+++ Run/MainApplication/Training/DetailTrainingRouter.swift
@@ -0,0 +1,24 @@
+//
+//  DetailTrainingRouter.swift
+//  Run
+//
+//  Created by Evgenii Kutasov on 30.10.2023.
+//
+
+import Foundation
+
+// MARK: Protocol - DetailTrainingPresenterToRouterProtocol (Presenter -> Router)
+protocol DetailTrainingPresenterToRouterProtocol: AnyObject {
+
+}
+
+class DetailTrainingRouter {
+
+    // MARK: Properties
+    weak var view: DetailTrainingRouterToViewProtocol!
+}
+
+// MARK: Extension - DetailTrainingPresenterToRouterProtocol
+extension DetailTrainingRouter: DetailTrainingPresenterToRouterProtocol {
+    
+}
