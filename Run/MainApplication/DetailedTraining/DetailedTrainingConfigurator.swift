566209767d71c8dcbe0dee611484286ba060e25f
EvgenLK <120525967+EvgenLK@users.noreply.github.com>
2023-12-11 22:03:42 +1000

    правки ошибок

--- Run/MainApplication/DetailedTraining/DetailedTrainingConfigurator.swift
+++ Run/MainApplication/DetailedTraining/DetailedTrainingConfigurator.swift
@@ -11,7 +11,7 @@ final class DetailedTrainingConfigurator {
     func configure(with idTrainingItem: TrainingCellViewModel) -> UIViewController {
         logger.log("\(#fileID) -> \(#function)")
         let view = DetailedTrainingViewController()
-        let presenter = DetailedTrainingPresenter() // не уверен что это правильно, может стоило передать в интерактор?
+        let presenter = DetailedTrainingPresenter()
         let router = DetailedTrainingRouter()
         let interactor = DetailedTrainingInteractor(with: idTrainingItem)
         
